29a683f5df940e001e679ab3cf5774a59853210a
unknown <shirleyapril23@gmail.com>
2023-10-04 16:18:27 +0300

    test post request

--- api/index.js
+++ api/index.js
@@ -6,12 +6,13 @@ app.get("/api", (req, res) => {
   res.send("Home route!");
 });
 
-app.get("/api/test", (req, res) => {
-    res.send("Test route!");
-  });
+app.post("/api/post", (req, res) => {
+  const body = { status: 200, message: "POST Request" };
+  res.json(body);
+});
 
 const PORT = 3005;
 
 app.listen(PORT, () => console.log("App running on port" + PORT));
 
-module.exports = app
+module.exports = app;
