1ebb074904923e26417826c3815dff3b01ec3375
unknown <shirley@ipayafrica.com>
2023-10-04 15:51:52 +0300

    initial commit

---
+++ api/index.js
@@ -0,0 +1,13 @@
+
+const express = require("express")
+
+const app = express()
+
+
+app.get("/", (req, res) => {
+    res.send("Home route!")
+})
+
+const PORT = 3005
+
+app.listen(PORT, () => console.log("App running on port" + PORT))
