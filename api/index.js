38ac9656bc3a4658a9d273a87bab6494c51dc009
unknown <shirleyapril23@gmail.com>
2023-10-05 09:06:48 +0300

    test serving static files

--- api/index.js
+++ api/index.js
@@ -1,19 +1,24 @@
 const express = require("express");
-var bodyParser = require('body-parser')
-
-app.use(express.static('public'))
+var bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
+app.use(express.static("public"));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.get("/api", (req, res) => {
   res.send("Home route!");
 });
 
 app.post("/api/post", (req, res) => {
-  const body = req.body
+  const body = req.body;
   res.json(body);
+});
+
+app.get("/api/file", (req, res) => {
+  const file_path = path.join(__dirname, "/public");
+  res.sendFile("DStv-logo.jpg", { root: file_path });
 });
 
 const PORT = 3005;
