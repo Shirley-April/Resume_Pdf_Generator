06acee9f1ebbfa0d290c827098c3c773d804d401
unknown <shirleyapril23@gmail.com>
2023-10-05 09:29:01 +0300

    add resume header section

--- api/create_resume.js
+++ api/create_resume.js
@@ -7,7 +7,7 @@ const createResume = (resume) => {
 
     generateHeader(resume, doc);
 
-    const path = `resume.pdf`;
+    const path = `${__dirname}/resume.pdf`;
 
     doc.end();
     const stream = doc.pipe(fs.createWriteStream(path));
@@ -21,3 +21,17 @@ const createResume = (resume) => {
     });
   });
 };
+
+const bold_font = `${__dirname}/public/Roboto-Bold.ttf`
+const regular_font = `${__dirname}/public/Roboto-Regular.ttf`
+
+const generateHeader = (resume, doc) => {
+  doc.fontSize(24).text(`${resume.contact.name}`);
+  doc
+    .font(regular_font)
+    .fontSize(10)
+    .text(`${resume.contact.jobTitle}`);
+  doc.moveDown(2);
+};
+
+module.exports = { createResume };
