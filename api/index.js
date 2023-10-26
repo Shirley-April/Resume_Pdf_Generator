94d1188a9c5a50ed2be0d8a008544c391abed8d3
unknown <shirleyapril23@gmail.com>
2023-10-26 13:14:59 +0300

    return the resume link as response

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 var bodyParser = require("body-parser");
-const path = require("path")
+const path = require("path");
 
 const { createResume } = require("./create_resume");
+const { uploadFile } = require("./dropbox/uploadFile");
+const { downloadFile } = require("./dropbox/downloadFile");
 
 const app = express();
 app.use(express.static("public"));
@@ -18,10 +20,15 @@ app.post("/api/file", async (req, res) => {
 
   try {
     const filePath = await createResume(resume);
-    const current_dir = __dirname
-    const parent_dir = path.dirname(current_dir)
 
-    res.sendFile(filePath, { root: parent_dir });
+    const upload_file = await uploadFile(filePath);
+    const download_file_link = await downloadFile();
+
+    if (upload_file.status === 200) {
+      res.status(200).send({
+        response: download_file_link,
+      });
+    }
   } catch (error) {
     res.status(500).send("Error creating the file: " + error.message);
   }
